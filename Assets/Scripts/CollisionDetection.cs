60936860ed4d72baaf47f1e0d8903ff04a6b0114
Pranav Mujumdar <psmujumdar@gmail.com>
2020-10-28 21:49:26 -0400

    Environment complete with brick spawn and physics

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    public PaddleAgent agent;
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.CompareTag("DeadZone"))
+        {
+            //Debug.Log("Dead");
+            //Debug.Log(collision.gameObject.tag);
+            agent.Died();
+        }
+        if (collision.gameObject.CompareTag("Brick"))
+        {
+            //Debug.Log("Dead");
+            // Debug.Log(collision.gameObject.tag);
+            agent.Scored();
+        }
+        if (collision.gameObject.CompareTag("Paddle"))
+        {
+            agent.hitPaddle();
+        }
+    }
+
+}
